a1aaf219eb112658732edf76392006e3f803d920
Shivaji Tanpure <shivajitanpure@Atoss-Mac-mini.local>
2018-07-02 15:55:45 +0530

    first commit

---
+++ SizeClasses/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SizeClasses
+//
+//  Created by Shivaji Tanpure on 17/05/18.
+//  Copyright © 2018 Shivaji Tanpure. All rights reserved.
+//
+
+//REF: https://www.youtube.com/watch?v=VLh0RIQmCr4  Size Classes are mainly used for -Layouts & Constraits for specific type of devices
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
